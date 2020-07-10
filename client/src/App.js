772272ded2f0b765f196414ff42b143d5ed06288
Sigi Jaeckel <sigij55@gmail.com>
2020-07-10 12:39:35 -0400

    added color toggle

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import PlantList from "./components/PlantList";
 import ShoppingCart from "./components/ShoppingCart";
 import CheckoutForm from "./components/CheckoutForm";
+import {useDarkMode} from './hooks/useDarkMode';
 
 import "./App.css";
 
@@ -19,6 +20,14 @@ function App() {
   // remove a plant from the cart
   const removeFromCart = (plant) => {
     setCart(cart.filter((p) => p.id !== plant.id));
+  };
+
+  //darkMode
+
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
   };
 
   return (
@@ -42,6 +51,9 @@ function App() {
                 </span>
               </NavLink>
             </li>
+            <div className='toggle-button'>
+              <p onClick={toggleMode} className= {darkMode ? 'toggle toggled' : 'toggle'} >Toggle Dark Mode</p>
+            </div>
           </ul>
         </nav>
         <Route
---
+++ client/src/hooks/useDarkMode.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react'
+import {useLocalStorage} from './useLocalStorage'
+
+
+
+export const useDarkMode = () => {
+    const [storedValue, setValue] = useLocalStorage('darkMode', false);
+
+    useEffect(() => {
+        const form = document.querySelector('form')
+        
+        if (storedValue === true) {
+            document.body.classList.add('dark-mode')
+            form.classList.add('dark-form')
+        } if (storedValue === false) {
+            document.body.classList.remove('dark-mode')
+            form.classList.remove('dark-form')
+        }
+    }, [storedValue]);
+
+    return [storedValue, setValue]
+}
